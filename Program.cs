671b62c756f472c20fb673a5baaf66797369c390
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-09-13 17:11:15 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Souza";
+p1.Idade = 40;
+p1.Apresentar();
+
+
+

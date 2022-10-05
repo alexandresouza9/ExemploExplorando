95a758d14e7e0ec72c0116585a417976adbd747b
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-10-05 09:17:08 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -8,5 +8,17 @@ p1.Sobrenome ="Silva";
 p1.Idade = 40;
 p1.Apresentar();
 
+Pessoa p2 = new Pessoa();
+p2.Nome = "Arthur";
+p2.Sobrenome = "Souza";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAluno();
+
 
 

7a19b79e128b67bd72b5dfe523246db84f562086
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-10-05 09:46:35 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -2,15 +2,14 @@
 //Console.WriteLine("Hello, World!");
 using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Souza";
-p1.Sobrenome ="Silva";
-p1.Idade = 40;
-p1.Apresentar();
-
-Pessoa p2 = new Pessoa();
-p2.Nome = "Arthur";
-p2.Sobrenome = "Souza";
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Souza";
+//p1.Sobrenome ="Silva";
+//p1.Idade = 40;
+//p1.Apresentar();
+Pessoa p1 = new Pessoa(nome:"Sonia", sobrenome: "Amancio");
+Pessoa p2 = new Pessoa("Arthur", "Souza");
+Pessoa p3 = new Pessoa("Alexandre", "Souza");
 
 Curso cursoDeIngles = new Curso();
 cursoDeIngles.Nome = "Ingles";
@@ -18,6 +17,7 @@ cursoDeIngles.Alunos = new List<Pessoa>();
 
 cursoDeIngles.AdicionarAluno(p1);
 cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
 cursoDeIngles.ListarAluno();
 
 
